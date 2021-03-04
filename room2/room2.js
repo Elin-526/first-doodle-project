bdbb61ddc53a2646f26fb6cf4d77bbde8f2b6a31
Elin-526 <79597513+Elin-526@users.noreply.github.com>
2021-03-04 11:42:38 -0500

    Add files via upload

---
+++ room2/room2.js
@@ -0,0 +1,17 @@
+
+// // Photos scrolling effects
+// const html =document.documentElement;
+// const canvas = document.querySelector('photo-scrolling');
+// const context = canvas.getContext('2d');
+
+// //Function Multipe Frames_Still Images
+
+// const currentFrame = index => (file:///Users/elin/Desktop/ELIN/1Ocadu/0_Ocadu%20Project/2021%20Winter/Graphic%20Design2/secondproject/project2/Photography_Room%202/1.JPG
+// )
+
+
+
+const scroller = new LocomotiveScroll({
+    el: document.querySelector('[data-scroll-container]'),
+    smooth: true
+  });
