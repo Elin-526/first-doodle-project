cbfb4692ae8dc00b1f7c5060d81e4599b0638cb8
Elin-526 <79597513+Elin-526@users.noreply.github.com>
2021-03-06 16:37:17 -0500

    Update room1.js

--- room1.js
+++ room1.js
@@ -1,6 +1,6 @@
  
 // Text_ Intro
-consoleText(['Welcome to the Room 1, Please feel free to scroll down and enter the each rooms', 'Explore your Doodling Skills', 'Made with Love.'], 'text',['black','yellow','white']);
+consoleText(['Welcome to the Room 1, Please feel free to scroll down and enter the each rooms', 'Explore your Doodling Skills', 'Made with Love.'], 'text',['black','dark yellow','white']);
 
 function consoleText(words, id, colors) {
 
