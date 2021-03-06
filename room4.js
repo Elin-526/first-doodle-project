25e650aa2acb471e070195c7315cef6e11b4a6c7
Elin-526 <79597513+Elin-526@users.noreply.github.com>
2021-03-06 12:29:06 -0500

    Update room4.js

--- room4.js
+++ room4.js
@@ -224,7 +224,7 @@ function setUpCanvas(){
     ctx = canvas.getContext("2d");
     canvas.width = w;
     canvas.height = h;
-    canvas.style.border = "1px solid black";
+    canvas.style.border = "1px solid white";
 }
 
 
